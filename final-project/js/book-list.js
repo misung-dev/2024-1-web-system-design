6c40a98ad9fed27ee99185f598ef426f77207f6b
misung.dev <misung.dev@gmail.com>
2024-06-17 12:51:10 +0900

    design: 도서 목록 사이드 및 탭 메뉴 디자인 일부 수정

--- final-project/js/book-list.js
+++ final-project/js/book-list.js
@@ -1,4 +1,10 @@
-function toggleSubmenu(id) {
-	const submenu = document.getElementById(id);
-	submenu.style.display = submenu.style.display === "block" ? "none" : "block";
-}
+document.addEventListener("DOMContentLoaded", () => {
+	const categoryButtons = document.querySelectorAll(".category button");
+
+	categoryButtons.forEach((button) => {
+		button.addEventListener("click", () => {
+			categoryButtons.forEach((btn) => btn.classList.remove("active"));
+			button.classList.add("active");
+		});
+	});
+});
