b670aba0cea9a812548af4f262d8fefa6880b858
misung.dev <misung.dev@gmail.com>
2024-06-18 15:29:41 +0900

    feat: url 복사 기능 구현

--- final-project/js/book-detail.js
+++ final-project/js/book-detail.js
@@ -1,5 +1,23 @@
+// 도서 서브 이미지 hover 효과 적용
 document.querySelectorAll(".book-img-sub img").forEach(function (img) {
 	img.addEventListener("mouseover", function () {
 		document.getElementById("main-img").src = this.src;
 	});
 });
+
+// 링크 외부로 공유
+function copyUrl() {
+	const url = window.location.href;
+	navigator.clipboard.writeText(url).then(
+		function () {
+			const notification = document.getElementById("copy-notification");
+			notification.style.display = "block";
+			setTimeout(function () {
+				notification.style.display = "none";
+			}, 2000);
+		},
+		function (err) {
+			console.error("URL 복사 실패: ", err);
+		}
+	);
+}
